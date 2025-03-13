86b9dee12659b4557a8412428a4bfeaee69f2941
Steven Florez <cristian_sflorez@soy.sena.edu.co>
2025-03-12 20:41:57 -0500

    Prooof1

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 
-// Permitir CORS si es necesario
-app.use(cors());
-
-// Servir archivos estáticos desde dist
+// server static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Para cualquier ruta, servir index.html (manejo de errores incluido)
-app.get('*', (req, res, next) => {
-  const indexPath = path.join(__dirname, 'dist', 'index.html');
-  res.sendFile(indexPath, (err) => {
-    if (err) {
-      next(err);
-    }
-  });
+// for any request that doesn't match a static file, serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Definir el puerto
+// Expose
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(Server running on port ${PORT});
 });
