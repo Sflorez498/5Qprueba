14a6e5c0e2b2ed9d63599a4bbe65a0d750403391
Steven Florez <cristian_sflorez@soy.sena.edu.co>
2025-03-12 20:47:37 -0500

    Prooof1.0

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('*', (req, res) => {
 // Expose
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
